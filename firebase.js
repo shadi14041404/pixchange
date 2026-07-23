1ae042cd0a4020eb9e7bd92042c35946c80876bf
shadi14041404 <shosho25-3@hotmail.com>
2026-07-23 07:32:10 +0300

    Create firebase.js with configuration template
    Added Firebase configuration template with placeholders.

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Firebase سيتم تفعيله لاحقًا
+
+const firebaseConfig = {
+
+apiKey: "YOUR_API_KEY",
+
+authDomain: "YOUR_PROJECT.firebaseapp.com",
+
+projectId: "YOUR_PROJECT",
+
+storageBucket: "YOUR_PROJECT.appspot.com",
+
+messagingSenderId: "000000000000",
+
+appId: "YOUR_APP_ID"
+
+};
+
+// سيتم وضع أكواد Firebase هنا بعد إنشاء المشروع
